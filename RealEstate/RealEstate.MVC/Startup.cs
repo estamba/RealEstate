af1441da848b08d416fc7ae291fa4168c0f66a14
estamba <princearbby97@gmail.com>
2020-08-23 02:59:57 +0800

    added docker

--- RealEstate/RealEstate.MVC/Startup.cs
+++ RealEstate/RealEstate.MVC/Startup.cs
@@ -122,7 +122,11 @@ namespace RealEstate.MVC
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-
+            app.Use((context, next) =>
+            {
+                context.Request.Scheme = "https";
+                return next();
+            });
             app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseCookiePolicy();
